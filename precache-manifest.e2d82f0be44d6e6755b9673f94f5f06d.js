99d522a224975239c9264b0b7fd70e9ee3a02e88
Fncteam <fncdevteam@gmail.com>
2020-12-30 16:15:37 +0530

    Updates

---
+++ precache-manifest.e2d82f0be44d6e6755b9673f94f5f06d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "17434c9374835d9eb1dc",
+    "url": "/flower-shop-master/static/css/main.e854c312.chunk.css"
+  },
+  {
+    "revision": "17434c9374835d9eb1dc",
+    "url": "/flower-shop-master/static/js/main.fb4545eb.chunk.js"
+  },
+  {
+    "revision": "7275109175ae414f1e69",
+    "url": "/flower-shop-master/static/js/runtime~main.29f71427.js"
+  },
+  {
+    "revision": "2514c8af5256606c01ba",
+    "url": "/flower-shop-master/static/css/2.2109ebfd.chunk.css"
+  },
+  {
+    "revision": "2514c8af5256606c01ba",
+    "url": "/flower-shop-master/static/js/2.392c5a11.chunk.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/flower-shop-master/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "a8d9f58038c81fe8547435eaa63915b5",
+    "url": "/flower-shop-master/static/media/FNC_logo.a8d9f580.jpg"
+  },
+  {
+    "revision": "dcbd4be4e68d0031c33405859af7977e",
+    "url": "/flower-shop-master/index.html"
+  }
+];
